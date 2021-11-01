dc6520ccf30f4b49dc0a35d5f5a000efa58a3722
Choihyunsu <chs991209@gmail.com>
2021-11-01 18:39:13 +0900

    Test용 DB Class 삭제

--- app.py
+++ app.py
@@ -11,13 +11,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
-class Test(db.Model):
-    __tablename__ = 'test_table'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), unique=True)
-
-
 db.create_all()
 
 
