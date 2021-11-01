cc771ec8d0ba945975c2c1a4296817642e09493c
Choihyunsu <chs991209@gmail.com>
2021-11-01 17:13:34 +0900

    SQLALCHEMY의 basedir 'app' 설정, app config 생성

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, 'db.sqlite')
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello():
